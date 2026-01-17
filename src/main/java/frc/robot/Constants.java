fc3021fb3e2fbff5c794f6af066774457a934255
asher <asher.fix@hopkinsrobotics.org>
2026-01-17 14:07:16 -0600

    Made Intake Subsystems File

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,45 +13,7 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    //TODO: update all variables ending in CANID
-
-    //Popcorn Constants
-    public static final double accelerationMotorTopSpeed = 0.85;
-    public static final double loadingMotorTopSpeed = 0.4;
-    public static final int popcornAcceleratorCANID = 10; //TODO: Put in the CANID's
-    public static final int popcornLoaderCANID = 28; //TODO: Put in the CANID's
-
-
-    //IntakeSubsystem Constants
-    public static final int intakeMotorCANID = 13;//was 13
-    public static final int winchMotorCANID = 1099;// was 10
-    public static final double winchMotorTopSpeed = 1; //was 1
-    public static final double butterIntakeTopSpeed = -0.3;    //was .6
-    public static final double butterOuttakeTopSpeed = 1.0;    //was .6
-
-    //PI be like...
-    public static final double pi = 3.14;
-
-    //the speed that the butter systems can run
-    //UPDATE ME!!!!
-    public static final double pullyLowerTime = 1;
-    public static final double pullyRaiseTime = 1;
-
-    // The CAN ID is the device ID of each sof the motors
-    public static final int rightLeaderCANID = 8;
-    public static final int rightFollowerCANID = 7;
-    public static final int leftLeaderCANID = 5;
-    public static final int leftFollowerCANID = 6;
-    //The controller ports can be changed in driverstation
-    public static final int driverXboxControllerPort = 0;
+   
     public static final int operatorXboxControllerPort = 1;
 
-    /* 
-    Sets the maximum power we can drive at. 1.0 is 100%. 
-    The number is negative because xbox controllers are down-right positve
-    */
-    public static final double maxMotorOutput = -0.8;
-
-    //butter intake
-    
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,11 +13,10 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.DriveSubsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.DriveCommands;
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -26,9 +25,9 @@ import frc.robot.DriveCommands;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+ 
 
-  private final CommandXboxController driveController = new CommandXboxController(Constants.driverXboxControllerPort);
+ 
   
   private final CommandXboxController operatorController = new CommandXboxController(Constants.operatorXboxControllerPort);
 
@@ -36,14 +35,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    driveSubsystem.setDefaultCommand(
-      new RunCommand(
-        () -> {
-          driveSubsystem.drive(Constants.maxMotorOutput*driveController.getLeftY(),
-          Constants.maxMotorOutput*driveController.getRightY());
-        }
-        , driveSubsystem)
-        );}
+  }
         /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -64,10 +56,5 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return new SequentialCommandGroup(
-      DriveCommands.drive(driveSubsystem, -.8, -.8).withTimeout(1.5)
-    );
-  }
+  
 }
