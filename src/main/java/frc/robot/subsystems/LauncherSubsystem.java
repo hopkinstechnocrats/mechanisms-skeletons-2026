99ff028e8e5b2dcfb4dd631e8d2350267014c7ef
asher <asher.fix@hopkinsrobotics.org>
2026-01-20 20:04:38 -0600

    Changed WPI_TalonSRX to TalonFX

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -1,18 +1,17 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
     public class LauncherSubsystem extends SubsystemBase{
-        WPI_TalonSRX launcherMotor;
+        TalonFX launcherMotor;
         public LauncherSubsystem(){
-            launcherMotor = new WPI_TalonSRX(Constants.launcherMotorCANID); //Need to getCANID
-            launcherMotor.configFactoryDefault();
-            launcherMotor.setNeutralMode(NeutralMode.Brake);
+            launcherMotor = new TalonFX(Constants.launcherMotorCANID); //Need to getCANID
+            launcherMotor.setNeutralMode(NeutralModeValue.Brake);
         }
     
         public void launcher(double launcherSpeed){
