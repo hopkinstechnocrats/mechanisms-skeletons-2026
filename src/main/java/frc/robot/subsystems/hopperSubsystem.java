c48e99d3be2d9a9e1995ef733c6895723e2d3394
Andre Nasses <Andre.Nasses@hopkinsrobotics.org>
2026-01-20 19:16:44 -0600

    added some hopper code

---
+++ src/main/java/frc/robot/subsystems/hopperSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.MotControllerJNI;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class hopperSubsystem extends SubsystemBase{
+    
+    WPI_TalonSRX hopperFeederMotor;
+
+    public hopperSubsystem(){
+        hopperFeederMotor = WPI_TalonSRX();
+        
+
+        
+    }
+
+}
