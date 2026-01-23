b6e1033417a5c96de330ac7614015058dd98318b
asher <asher.fix@hopkinsrobotics.org>
2026-01-22 19:31:57 -0600

    added a negative to the outtake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,5 +17,5 @@ public final class Constants {
     public static final int operatorXboxControllerPort = 1;
     public static final int intakeMotorCANID = 2;//CHANGE THIS
     public static final double intakeSpeed = .1;
-    public static final double outtakeSpeed = .1;
+    public static final double outtakeSpeed = -.1;
 }
