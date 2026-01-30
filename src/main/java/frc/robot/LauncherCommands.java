2c81b0317c9ef7c31b7e99011922aac51eb068f8
Marilyn <136532458+Freezerat@users.noreply.github.com>
2026-01-29 19:22:36 -0600

    fixed build error

--- src/main/java/frc/robot/LauncherCommands.java
+++ src/main/java/frc/robot/LauncherCommands.java
@@ -1,8 +1,7 @@
-package frc.robot.Commands;
+package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Constants;
 import frc.robot.subsystems.LauncherSubsystem;
 
 public class LauncherCommands extends Command {
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.LauncherSubsystem;
-import frc.robot.Commands.LauncherCommands;
+// import frc.robot.Commands.LauncherCommands;
+import frc.robot.LauncherCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -37,9 +38,9 @@ public class RobotContainer {
       launcherSubsystem.setDefaultCommand(
             new RunCommand(
                     () -> {
-                    launcherSubsystem.launcher(Constants.launcherBrakeSpeed);
-                  }, launcherSubsystem)
-    );
+                    launcherSubsystem.launcherBrake(Constants.launcherBrakeSpeed);
+                  }, launcherSubsystem
+      ));
       
   }
         /**
