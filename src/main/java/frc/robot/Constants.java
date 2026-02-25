7f8b83ad5ecf803828d54d9a58e5c78e81bf3a55
Andre <Andre.Nasses@hopkinsrobotics.org>
2026-02-24 20:23:02 -0600

    updated canids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,8 +68,8 @@ public final class Constants{
     }
 
     public static final class LauncherConstants{
-        public static final int k_launcherMotorSecondCANID = 11;
-        public static final int k_launcherMotorCANID = 12;
+        public static final int k_launcherMotorSecondCANID = 21;
+        public static final int k_launcherMotorCANID = 20;
         public static final double k_launcherSpeedRPS = 0.5;
         public static final double k_reverseLauncherSpeedRPS = -0.5;   
         public static final double k_launcherBrakeSpeedRPS = 0;
